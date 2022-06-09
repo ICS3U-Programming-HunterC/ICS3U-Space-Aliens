3454bb8618c685f85ba8925c9352b7448c8e0146
HunterConnolly <99264225+HunterConnolly@users.noreply.github.com>
2022-06-09 09:45:48 -0400

    worked

--- code.py
+++ code.py
@@ -1,1 +1,36 @@
-print("Hello Hunter!")
+#!/usr/bin/env python
+# Created by: Hunter Connolly
+# Created on June 2022
+# This program is the "Space Aliens" program on the PyBadge
+
+import ugame
+import stage
+
+def game_scene():
+    # this function is the main game scene
+
+    # image banks for CircuitPython
+    image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    
+    # set the backgorund image to 0 in the image bank
+    # and the size (10x8 tiles of size 16x16)
+    background = stage.Grid(image_bank_background, 10, 8)
+
+    # create a stage for the background to show up on
+    # and set the frame rate to 60fps
+    game = stage.Stage(ugame.display, 60)
+    
+    # set the layers of all sprites, items show up in order
+    game.layers = [background]
+    
+    # render all sprites
+    # most likely you will only render the background once per game scene
+    game.render_block()
+
+    # repeat forever, game loop
+    while True:
+        # repeat forever, or you turn it off
+        pass # just a place holder
+
+if __name__ == "__main__":
+    game_scene
