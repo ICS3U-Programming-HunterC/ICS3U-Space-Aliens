6668c5572a3105487015a2a4c46ec49a2077e148
HunterConnolly <99264225+HunterConnolly@users.noreply.github.com>
2022-06-13 09:11:08 -0400

    added sprite

--- code.py
+++ code.py
@@ -11,17 +11,20 @@ def game_scene():
 
     # image banks for CircuitPython
     image_bank_background = stage.Bank.from_bmp16("space_aliens_background.bmp")
+    image_bank_sprites = stage.Bank.from_bmp16("space_aliens.bmp")
     
     # set the backgorund image to 0 in the image bank
     # and the size (10x8 tiles of size 16x16)
     background = stage.Grid(image_bank_background, 10, 8)
+
+    ship = stage.Sprite(image_bank_sprites, 5, 75, 66)
 
     # create a stage for the background to show up on
     # and set the frame rate to 60fps
     game = stage.Stage(ugame.display, 60)
     
     # set the layers of all sprites, items show up in order
-    game.layers = [background]
+    game.layers = [ship] + [background]
     
     # render all sprites
     # most likely you will only render the background once per game scene
@@ -29,8 +32,13 @@ def game_scene():
 
     # repeat forever, game loop
     while True:
-        # repeat forever, or you turn it off
-        pass # just a place holder
+        # get user input
+
+        # update game logic
+
+        # redraw Sprites
+        game.render_sprites([ship])
+        game.tick
 
 if __name__ == "__main__":
     game_scene()
